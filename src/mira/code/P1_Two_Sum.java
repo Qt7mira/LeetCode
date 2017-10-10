b1c9f2a69dfec504e5e9aea0680f28bbed67326a
qt7mira <771307676@qq.com>
2017-10-10 19:53:30 +0800

    优化P1代码

--- src/mira/code/P1_Two_Sum.java
+++ src/mira/code/P1_Two_Sum.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 public class P1_Two_Sum {
 
 	/*
-	 * 题目：
-	 * Given an array of integers, return indices of the two numbers such that
-	 * they add up to a specific target.
+	 * 题目： Given an array of integers, return indices of the two numbers such
+	 * that they add up to a specific target.
 	 * 
 	 * You may assume that each input would have exactly one solution, and you
 	 * may not use the same element twice.
@@ -18,29 +17,25 @@ public class P1_Two_Sum {
 	 */
 
 	/*
-	 * 创建一个哈希表，在遍历的同时寻找target-i
-	 * 时间复杂度和空间复杂度都是O(n)
+	 * 创建一个哈希表，在遍历的同时寻找target-nums[i]. 时间复杂度和空间复杂度都是O(n).
 	 */
 
 	public int[] twoSum(int[] nums, int target) {
 
-		if (nums == null || nums.length == 0) {
-			return null;
-		}
+		// if (nums == null || nums.length == 0) {
+		// return null;
+		// }
 
-		int[] result = new int[2];
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
 		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(target - nums[i])) {
-				result[0] = map.get(target - nums[i]);
-				result[1] = i;
-				break;
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] { map.get(complement), 1 };
 			} else {
 				map.put(nums[i], i);
 			}
-
 		}
-		return result;
+		throw new IllegalArgumentException("No two sum solution");
 	}
 }
