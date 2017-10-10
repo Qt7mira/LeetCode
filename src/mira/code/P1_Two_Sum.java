976e7143afef2e8d5e84e7ce9d1c11c62e0ff4f0
qt7mira <771307676@qq.com>
2017-10-10 19:41:36 +0800

    P1 注释

--- src/mira/code/P1_Two_Sum.java
+++ src/mira/code/P1_Two_Sum.java
@@ -4,6 +4,24 @@ import java.util.HashMap;
 
 public class P1_Two_Sum {
 
+	/*
+	 * 题目：
+	 * Given an array of integers, return indices of the two numbers such that
+	 * they add up to a specific target.
+	 * 
+	 * You may assume that each input would have exactly one solution, and you
+	 * may not use the same element twice.
+	 * 
+	 * Example: Given nums = [2, 7, 11, 15], target = 9,
+	 * 
+	 * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+	 */
+
+	/*
+	 * 创建一个哈希表，在遍历的同时寻找target-i
+	 * 时间复杂度和空间复杂度都是O(n)
+	 */
+
 	public int[] twoSum(int[] nums, int target) {
 
 		if (nums == null || nums.length == 0) {
